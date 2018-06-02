5e682cd8706f17d9a271d49961c64805113f3c40
Morten Knaack <knaack@gmail.com>
2018-06-03 00:02:24 +0200

    Added create to integration test and refactored authorisation and list/create into modules

--- integration_test/test.ml
+++ integration_test/test.ml
@@ -4,15 +4,32 @@ open Databases
 
 (* list_databases *)
 
-let p = list_databases endpoint
+module MyAuthKeys : Auth_key = struct
+  let master_key = "SB1mrDcsPfPnHN2lCLYLTXDJMEqXsjvWqS2BXbvBbro94dxVHem3gyXKLPruSeMVE7ZKf36EGC5ArCkJqJaoOg=="
+  let endpoint = "mknnack"
+end
+
+module MyAuth = Auth(MyAuthKeys)
+
+module D = Database(MyAuth)
+
+let p = D.list_databases ()
 let px = p >>= content
 let result = Lwt_main.run px
-let dbs = convert_list_databases result
+let dbs = D.convert_list_databases result
 (* let dbs_names = List.map (fun x -> x) dbs.databases *)
 (* let xxx = List.map (fun x -> x.) dbs_names *)
 let _ = print_string result
 (* let dbs_names =  List.map (fun x -> x.id) dbs.databases *)
 
+
+(* create database *)
+
+let p = D.create "test"
+let px = p >>= content
+
+let result = Lwt_main.run px
+let _ = print_string result
 
 
 
--- src/databases.ml
+++ src/databases.ml
@@ -8,16 +8,36 @@ open Yojson
 *)
 
 (* let authorization_token_using_master_key = Utility.authorization_token_using_master_key *)
-  
-let authorization date = (* "type=master&ver=1.0&sig=" ^ key *)
-  let master_key = "SB1mrDcsPfPnHN2lCLYLTXDJMEqXsjvWqS2BXbvBbro94dxVHem3gyXKLPruSeMVE7ZKf36EGC5ArCkJqJaoOg==" in
-  let verb = "GET" in (* get, post, put *)
-  let resource_type = "dbs" in (* "dbs", "colls", "docs". *)
-  (* let resource_id = "dbs/mknnack" in *)
-  let resource_id = "" in
-  let result = Utility.authorization_token_using_master_key verb resource_type resource_id date master_key in
-  result
-let endpoint = "mknnack"
+
+module type Auth_key = sig
+  val master_key : string
+  val endpoint : string
+end
+
+module type Account = sig
+  type verb = Get | Post | Put
+  val authorization : verb -> string -> string
+  val endpoint : string
+end
+
+module Auth (Keys : Auth_key) : Account = struct
+  type verb = Get | Post | Put
+
+  let string_of_verb = function
+    | Get -> "GET"
+    | Post -> "POST"
+    | Put -> "PUT"
+
+  let authorization verb date = (* "type=master&ver=1.0&sig=" ^ key *)
+    let verb = string_of_verb verb in (* get, post, put *)
+    let resource_type = "dbs" in (* "dbs", "colls", "docs". *)
+    (* let resource_id = "dbs/mknnack" in *)
+    let resource_id = "" in
+    let result = Utility.authorization_token_using_master_key verb resource_type resource_id date Keys.master_key in
+    result
+
+  let endpoint = Keys.endpoint
+end
 
 let content = function
   | { Ocsigen_http_frame.frame_content = Some v } ->
@@ -27,131 +47,55 @@ let content = function
   | _ -> return ""
 
 (* list databases: *)
- 
-let list_databases databaseaccount =
-  let ms_date =
-    let now = Unix.time () in
-    Utility.x_ms_date now
-  in
-  let headers =
-    Http_headers.empty
-  |> Http_headers.add (Http_headers.name "authorization") (authorization ms_date)
-  |> Http_headers.add (Http_headers.name "x-ms-version") "2017-02-22"
-  |> Http_headers.add (Http_headers.name "x-ms-date") ms_date
-  in
-  let get = Ocsigen_http_client.get
-      ~https:true
-      ~host: (databaseaccount ^ ".documents.azure.com")
-      ~uri:"/dbs"
-      ~headers
-      ~port:443
-      ()
-  in
-  get
 
-let convert_list_databases s =
-  Json_converter_j.list_databases_of_string s
-
-
-(* (\* create database: *\) *)
-
-
-(* let authorization date = (\* "type=master&ver=1.0&sig=" ^ key *\) *)
-(*   let master_key = "SB1mrDcsPfPnHN2lCLYLTXDJMEqXsjvWqS2BXbvBbro94dxVHem3gyXKLPruSeMVE7ZKf36EGC5ArCkJqJaoOg==" in *)
-(*   let verb = "POST" in (\* get, post, put *\) *)
-(*   let resource_type = "dbs" in (\* "dbs", "colls", "docs". *\) *)
-(*   (\* let resource_id = "dbs/mknnack" in *\) *)
-(*   let resource_id = "" in *)
-(*   let result = authorization_token_using_master_key verb resource_type resource_id date master_key in *)
-(*   result *)
-(* let endpoint = "mknnack" *)
-
-(*   (\* a simple function to access the content of the response *\) *)
-(* (\* let content = function *\) *)
-(* (\*   | { Ocsigen_http_frame.frame_content = Some v } -> *\) *)
-(* (\*       let r = Ocsigen_stream.string_of_stream 100000 (Ocsigen_stream.get v) in *\) *)
-(* (\*       let _ = Ocsigen_stream.finalize v in *\) *)
-(* (\*       r *\) *)
-(* (\*   | _ -> return "" *\) *)
-
-(* (\* let json_data = `Assoc [("a", `Int 3)] *\) *)
-
-(* (\* let json_content = *\) *)
-(* (\*   `List [ *\) *)
-(* (\*   `Assoc [ *\) *)
-(* (\*   ("eventId", `String "fbf4a1a1-b4a3-4dfe-a01f-ec52c34e16f7"); *\) *)
-(* (\*   ("eventType", `String "event-type"); *\) *)
-(* (\*   (\\* ("data", `Assoc [("a", `Int 5)]); *\\) *\) *)
-(* (\*   ("data", json_data); *\) *)
-(* (\* ] *\) *)
-(* (\* ] *\) *)
-
-(* (\* let post_content = Yojson.pretty_to_string json_content *\) *)
-
-(* (\* let _ = print_endline post_content *\) *)
-    
-(* (\* let content_type = "application", "json" *\) *)
-    
-(* (\* let p = Ocsigen_http_client.post_string ~host:"localhost" ~uri:"/streams/newstream" ~port:2113 ~content:post_content ~content_type:content_type () *\) *)
-(* (\* let px = p >>= content   *\) *)
-
-(* (\* let result = Lwt_main.run px *\) *)
-(* (\* let _ = print_string result *\) *)
-(* let content = function *)
-(*   | { Ocsigen_http_frame.frame_content = Some v } -> *)
-(*       let r = Ocsigen_stream.string_of_stream 100000 (Ocsigen_stream.get v) in *)
-(*       let _ = Ocsigen_stream.finalize v in *)
-(*       r *)
-(*   | _ -> return "" *)
-
-(* (\* let list_databases = *\) *)
+module Database (Account : Account) = struct
+  let headers verb =
+    let ms_date =
+      let now = Unix.time () in
+      Utility.x_ms_date now
+      in
+      Http_headers.empty
+      |> Http_headers.add (Http_headers.name "authorization") (Account.authorization verb ms_date)
+      |> Http_headers.add (Http_headers.name "x-ms-version") "2017-02-22"
+      |> Http_headers.add (Http_headers.name "x-ms-date") ms_date
   
+  let list_databases () =
+    let headers = headers Account.Get in
+    let get = Ocsigen_http_client.get
+        ~https:true
+        ~host: (Account.endpoint ^ ".documents.azure.com")
+        ~uri:"/dbs"
+        ~headers
+        ~port:443
+        ()
+    in
+    get
 
-  
-(* let create databaseaccount name = *)
-(*   let post_content = *)
-(*     let json_content = *)
-(*       `Assoc [ *)
-(*       ("id", `String name); *)
-(*     ] *)
-(*     in *)
-(*     Yojson.pretty_to_string json_content *)
-(*   in *)
-(*   let content_type = "application", "json" in *)
-(*   let ms_date = *)
-(*     let now = Unix.time () in *)
-(*     Utility.x_ms_date now *)
-(*   in *)
-(*   let headers = *)
-(*     Http_headers.empty *)
-(*   |> Http_headers.add (Http_headers.name "authorization") (authorization ms_date) *)
-(*   |> Http_headers.add (Http_headers.name "x-ms-version") "2017-02-22" *)
-(*   |> Http_headers.add (Http_headers.name "x-ms-date") ms_date *)
-(*   in *)
-(*   let post = Ocsigen_http_client.post_string *)
-(*       ~https:true *)
-(*       ~host: (databaseaccount ^ ".documents.azure.com") *)
-(*       ~uri:"/dbs" *)
-(*       ~headers *)
-(*       ~port:443 *)
-(*       ~content:post_content *)
-(*       ~content_type *)
-(*       () *)
-(*   in *)
-(*   post *)
-    
-(* (\* let post_content = create "mknaack" "test" *\) *)
-(* (\* let _ = print_endline post_content *\) *)
-                                 
-(* (\* let content_type = "application", "vnd.eventstore.events+json" *\) *)
-                                 
-(* (\* let p = Ocsigen_http_client.post_string ~host:"localhost" ~uri:"/streams/newstream" ~port:2113 ~content:post_content ~content_type:content_type () *\) *)
+  let convert_list_databases s =
+    Json_converter_j.list_databases_of_string s
 
+  (* (\* create database: *\) *)
 
-
-
-(* let p = create endpoint "test" *)
-(* let px = p >>= content *)
-
-(* let result = Lwt_main.run px *)
-(* let _ = print_string result *)
+  let create name =
+    let post_content =
+      let json_content =
+        `Assoc [
+          ("id", `String name);
+        ]
+      in
+      Yojson.pretty_to_string json_content
+    in
+    let content_type = "application", "json" in
+    let headers = headers Account.Post in
+    let post = Ocsigen_http_client.post_string
+        ~https:true
+        ~host: (Account.endpoint ^ ".documents.azure.com")
+        ~uri:"/dbs"
+        ~headers
+        ~port:443
+        ~content:post_content
+        ~content_type
+        ()
+    in
+    post
+end
