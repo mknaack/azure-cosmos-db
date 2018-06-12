9f0828aa9f28094baffd35c2d71811017a517a09
Morten Knaack <knaack@gmail.com>
2018-06-12 22:51:12 +0200

    Work on collections. List works, create not working

--- integration_test/test.ml
+++ integration_test/test.ml
@@ -19,13 +19,17 @@ let do_command name  p =
   print_endline (name ^ ":");
   print_endline result
 
-let _ = do_command "create" (D.create dbname)
+let _ = do_command "create database" (D.create dbname)
 
-let _ = do_command "list_databases" (D.list_databases ())
+let _ = do_command "list databases" (D.list_databases ())
 
-let _ = do_command "get" (D.get dbname)
+let _ = do_command "create collection" (D.Collection.create dbname "testCollection")
 
-let _ = do_command "delete" (D.delete dbname)
+let _ = do_command "list collection" (D.Collection.list dbname)
+
+let _ = do_command "get database" (D.get dbname)
+
+let _ = do_command "delete database" (D.delete dbname)
 
 
 
--- src/databases.ml
+++ src/databases.ml
@@ -1,14 +1,6 @@
 open Lwt
 open Yojson
 
-
-(*
-- create database
-- list database
-*)
-
-(* let authorization_token_using_master_key = Utility.authorization_token_using_master_key *)
-
 module type Auth_key = sig
   val master_key : string
   val endpoint : string
@@ -16,23 +8,29 @@ end
 
 module type Account = sig
   type verb = Get | Post | Put | Delete
-  val authorization : verb -> string -> string -> string
+  type resource = Dbs | Colls | Docs
+  val authorization : verb -> resource -> string -> string -> string
   val endpoint : string
 end
 
 module Auth (Keys : Auth_key) : Account = struct
   type verb = Get | Post | Put | Delete
-
+  type resource = Dbs | Colls | Docs
+                  
   let string_of_verb = function
     | Get -> "GET"
     | Post -> "POST"
     | Put -> "PUT"
     | Delete -> "DELETE"
+
+  let string_of_resource = function
+    | Dbs -> "dbs"
+    | Colls -> "colls"
+    | Docs -> "docs"
   
-  let authorization verb date db_name = (* "type=master&ver=1.0&sig=" ^ key *)
+  let authorization verb resource date db_name = (* "type=master&ver=1.0&sig=" ^ key *)
     let verb = string_of_verb verb in (* get, post, put *)
-    let resource_type = "dbs" in (* "dbs", "colls", "docs". *)
-    (* let resource_id = "dbs/mknnack" in *)
+    let resource_type = string_of_resource resource in (* "dbs", "colls", "docs". *)
     let resource_id = db_name in
     let result = Utility.authorization_token_using_master_key verb resource_type resource_id date Keys.master_key in
     result
@@ -52,20 +50,20 @@ let convert_list_databases s =
   Json_converter_j.list_databases_of_string s
 
 module Database (Account : Account) = struct
-  let headers verb db_name =
+  let headers resource verb db_name =
     let ms_date =
       let now = Unix.time () in
       Utility.x_ms_date now
       in
       Http_headers.empty
-      |> Http_headers.add (Http_headers.name "authorization") (Account.authorization verb ms_date db_name)
+      |> Http_headers.add (Http_headers.name "authorization") (Account.authorization verb resource  ms_date db_name)
       |> Http_headers.add (Http_headers.name "x-ms-version") "2017-02-22"
       |> Http_headers.add (Http_headers.name "x-ms-date") ms_date
 
   let host = Account.endpoint ^ ".documents.azure.com"
   
   let list_databases () =
-    let headers = headers Account.Get in
+    let headers = headers Account.Dbs Account.Get in
     let get = Ocsigen_http_client.get
         ~https:true
         ~host
@@ -84,7 +82,7 @@ module Database (Account : Account) = struct
       Json_converter_j.string_of_create_database value
     in
     let content_type = "application", "json" in
-    let headers = headers Account.Post in
+    let headers = headers Account.Dbs Account.Post in
     let post = Ocsigen_http_client.post_string
         ~https:true
         ~host
@@ -98,7 +96,7 @@ module Database (Account : Account) = struct
     post
 
   let get name =
-    let headers = headers Account.Get in
+    let headers = headers  Account.Dbs Account.Get in
     let get = Ocsigen_http_client.get
         ~https:true
         ~host
@@ -124,7 +122,7 @@ module Database (Account : Account) = struct
       ()
   
   let delete name =
-    let headers = headers Account.Delete in
+    let headers = headers Account.Dbs Account.Delete in
     let command = delete
         ~https:true
         ~host
@@ -134,5 +132,39 @@ module Database (Account : Account) = struct
         ()
     in
     command
+
+  module Collection = struct
+    let list dbname =
+      let headers = headers Account.Colls Account.Get in
+      let get = Ocsigen_http_client.get
+          ~https:true
+          ~host
+          ~uri:("/dbs/" ^ dbname ^ "/colls")
+          ~headers: (headers ("dbs/" ^ dbname))
+          ~port:443
+        ()
+      in
+      get
+
     
+    let create dbname coll_name =
+      let post_content =
+        let value = ({id = coll_name; indexingPolicy = None; partitionKey = None}: Json_converter_j.create_collection) in
+        Json_converter_j.string_of_create_collection value
+      in
+      let content_type = "application", "json" in
+      let headers = headers Account.Colls Account.Post in
+      let post = Ocsigen_http_client.post_string
+          ~https:true
+          ~host
+          ~uri:("/dbs/" ^ dbname ^ "/colls")
+          ~headers: (headers ("dbs/" ^ dbname ^ "/colls"))
+          ~port:443
+          ~content:post_content
+          ~content_type
+          ()
+      in
+      post
+    
+  end
 end
