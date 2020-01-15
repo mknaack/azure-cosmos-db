4ff18b45cbfc945e4f2ed1bfb1e4e198ef04f011
Morten Knaack <knaack@gmail.com>
2020-01-15 16:38:51 +0100

    Remove mli file

--- src/Ocsigen_extra.mli
+++
@@ -1,2 +0,0 @@
- val delete : ?v6:bool -> ?https:bool -> ?port:int -> ?headers:Http_headers.t -> host:string -> uri:string -> unit -> Ocsigen_http_frame.t Lwt.t
- val put_string : ?v6:bool -> ?https:bool -> ?port:int -> ?headers:Http_headers.t -> host:string -> uri:string -> content: string -> content_type: (string * string) -> unit -> Ocsigen_http_frame.t Lwt.t
