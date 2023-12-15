02a80602bf49e1a3977252e2adca7cac1c77125c
Morten Knaack <knaack@gmail.com>
2023-12-15 22:55:52 +0100

    Reorganize tests

--- test/test.ml
+++ test/test.ml
@@ -2,8 +2,9 @@ let () =
   Lwt_main.run
   @@ Alcotest_lwt.run "Main tests"
        [
-         ("utility test", Test_utility.utility_test);
+         ("utility cosmos test", Test_cosmos_utility.utility_test);
          ("integration test", Integration_test.test);
          ("partition key test", Integration_test.test_partition_key);
          ("user test", Users_test.test);
+         ("utility test", Test_utilities.test);
        ]
--- test/utilities/test.ml
+++
@@ -1,3 +0,0 @@
-let () =
-  Lwt_main.run
-  @@ Alcotest_lwt.run "Main tests" [ ("utility test", Test_utilities.test) ]
