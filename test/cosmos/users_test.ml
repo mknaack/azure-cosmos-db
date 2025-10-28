67cf7d5668a79aededa949c4f921955de3d0a4f9
Morten Knaack <knaack@gmail.com>
2025-10-28 21:13:53 +0100

    Add timeout tests

--- test/cosmos/users_test.ml
+++ test/cosmos/users_test.ml
@@ -29,9 +29,18 @@ let create_user_test _ () =
       let _ = Alcotest.(check string) "Create name is correct" user_name id in
       return ()
 
+let create_user_timeout_test _ () =
+  let%lwt res = D.User.create ~timeout:0.0 dbname user_name in
+  match res with
+  | Result.Error Timeout_error ->
+      Alcotest.(check unit) "Timeout error" () ();
+      Lwt.return_unit
+  | Result.Error _ -> Alcotest.fail "Should fail with timeout error"
+  | Result.Ok _ -> Alcotest.fail "Should fail with timeout error"
+
 let list_user_test _ () =
-  let res = D.User.list dbname in
-  res >>= function
+  let%lwt res = D.User.list dbname in
+  match res with
   | Result.Error _ -> Alcotest.fail "Should not return error"
   | Result.Ok (code, { rid = _; users; count }) ->
       let db =
@@ -44,18 +53,36 @@ let list_user_test _ () =
       in
       return ()
 
+let list_user_timeout_test _ () =
+  let%lwt res = D.User.list ~timeout:0.0 dbname in
+  match res with
+  | Result.Error Timeout_error ->
+      Alcotest.(check unit) "Timeout error" () ();
+      Lwt.return_unit
+  | Result.Error _ -> Alcotest.fail "Should fail with timeout error"
+  | Result.Ok _ -> Alcotest.fail "Should fail with timeout error"
+
 let get_user_test _ () =
-  let res = D.User.get dbname user_name in
-  res >>= function
+  let%lwt res = D.User.get dbname user_name in
+  match res with
   | Result.Error _ -> Alcotest.fail "Should not return error"
   | Result.Ok (code, { id; _ }) ->
       let _ = Alcotest.(check int) "Status same int" 200 code in
       let _ = Alcotest.(check string) "Create name is correct" user_name id in
       return ()
 
+let get_user_timeout_test _ () =
+  let%lwt res = D.User.get ~timeout:0.0 dbname user_name in
+  match res with
+  | Result.Error Timeout_error ->
+      Alcotest.(check unit) "Timeout error" () ();
+      Lwt.return_unit
+  | Result.Error _ -> Alcotest.fail "Should fail with timeout error"
+  | Result.Ok _ -> Alcotest.fail "Should fail with timeout error"
+
 let replace_user_test _ () =
-  let res = D.User.replace dbname user_name replace_user_name in
-  res >>= function
+  let%lwt res = D.User.replace dbname user_name replace_user_name in
+  match res with
   | Result.Error _ -> Alcotest.fail "Should not return error"
   | Result.Ok (code, { id; _ }) ->
       let _ = Alcotest.(check int) "Status same int" 200 code in
@@ -64,17 +91,28 @@ let replace_user_test _ () =
       in
       return ()
 
+let replace_user_timeout_test _ () =
+  let%lwt res =
+    D.User.replace ~timeout:0.0 dbname user_name replace_user_name
+  in
+  match res with
+  | Result.Error Timeout_error ->
+      Alcotest.(check unit) "Timeout error" () ();
+      Lwt.return_unit
+  | Result.Error _ -> Alcotest.fail "Should fail with timeout error"
+  | Result.Ok _ -> Alcotest.fail "Should fail with timeout error"
+
 let delete_user_test _ () =
-  let res = D.User.delete dbname replace_user_name in
-  res >>= function
+  let%lwt res = D.User.delete dbname replace_user_name in
+  match res with
   | Result.Error _ -> Alcotest.fail "Should not return error"
   | Result.Ok code ->
       let _ = Alcotest.(check int) "Status same int" 204 code in
       return ()
 
 let delete_database_test _ () =
-  let res = D.delete dbname in
-  res >>= function
+  let%lwt res = D.delete dbname in
+  match res with
   | Result.Error _ -> Alcotest.fail "Should not return error"
   | Result.Ok code ->
       let _ = Alcotest.(check int) "Status same int" 204 code in
@@ -84,9 +122,14 @@ let user_tests =
   [
     Alcotest_lwt.test_case "create database" `Slow create_database_test;
     Alcotest_lwt.test_case "create user" `Slow create_user_test;
+    Alcotest_lwt.test_case "create user timeout" `Slow create_user_timeout_test;
     Alcotest_lwt.test_case "list user" `Slow list_user_test;
+    Alcotest_lwt.test_case "list user timeout" `Slow list_user_timeout_test;
     Alcotest_lwt.test_case "get user" `Slow get_user_test;
+    Alcotest_lwt.test_case "get user timeout" `Slow get_user_timeout_test;
     Alcotest_lwt.test_case "replace user" `Slow replace_user_test;
+    Alcotest_lwt.test_case "replace user timeout" `Slow
+      replace_user_timeout_test;
     Alcotest_lwt.test_case "delete user" `Slow delete_user_test;
     Alcotest_lwt.test_case "delete database" `Slow delete_database_test;
   ]
