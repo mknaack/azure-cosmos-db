718758b0fb2954cb80596faefaf70582e990a5a1
Docker <docker@example.com>
2026-03-06 22:39:56 +0000

    Remove test of non-partition

--- test/test.ml
+++ test/test.ml
@@ -4,7 +4,6 @@ let () =
        [
          ("app config test", Test_app_config.test);
          ("utility cosmos test", Test_cosmos_utility.utility_test);
-         ("integration test", Integration_test.test);
          ("partition key test", Integration_test.test_partition_key);
          ("user test", Users_test.test);
          ("permission test", Permission_test.test);
