0e56b2e614e02dc68f4058e444ba4d51141295ea
Morten Knaack <knaack@gmail.com>
2026-05-12 21:24:43 +0100

    Remove unused function and refactor

--- src/cosmos_eio/databases.mli
+++ src/cosmos_eio/databases.mli
@@ -263,7 +263,6 @@ module Database (Auth_key : Auth_key) : sig
         | Empty_batch
 
       val validate : operation list -> (unit, validation_error) result
-      val is_success : operation_result -> bool
 
       val execute :
         ?timeout:float ->
